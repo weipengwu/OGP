51fd526498d739f355c2562bc5accabaee10e4e7
Weipeng Wu <wwp722@gmail.com>
2015-10-30 00:15:11 -0400

    event map fix

--- resources/views/events/view.blade.php
+++ resources/views/events/view.blade.php
@@ -79,11 +79,23 @@
 		</div>
 	</div>
 </section>
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyANM_gBRmfXCbtGiN768aUL1div-Dd0TU4"></script>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyANM_gBRmfXCbtGiN768aUL1div-Dd0TU4&libraries=places"></script>
     <script type="text/javascript">
       function initialize() {
+      	var event_address = <?php echo $event->address;?>;
+            
+            var event_geocoder = new google.maps.Geocoder();
+            event_geocoder.geocode( { 'address': event_address}, function(results, status) {
+              if (status == google.maps.GeocoderStatus.OK) {
+                
+                eventlat = results[0].geometry.location.lat();
+                eventlng = results[0].geometry.location.lng();
+              } else {
+                alert("Geocode for Address was not successful for the following reason: " + status);
+              }
+            });
         var mapOptions = {
-          center: { lat: 43.6509618, lng: -79.3824327},
+          center: { lat: eventlat, lng: eventlng},
           zoom: 12,
           scrollwheel: false,
           styles: [{"featureType":"landscape","stylers":[{"hue":"#FFBB00"},{"saturation":43.400000000000006},{"lightness":37.599999999999994},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#FFC200"},{"saturation":-61.8},{"lightness":45.599999999999994},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":51.19999999999999},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":52},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#0078FF"},{"saturation":-13.200000000000003},{"lightness":2.4000000000000057},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#00FF6A"},{"saturation":-1.0989010989011234},{"lightness":11.200000000000017},{"gamma":1}]}]
