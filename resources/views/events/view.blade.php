8546e648835d8b850e63f44da751ea4efb301ab7
Weipeng Wu <wwp722@gmail.com>
2015-11-03 14:40:27 -0500

    map fix

--- resources/views/events/view.blade.php
+++ resources/views/events/view.blade.php
@@ -84,26 +84,28 @@
     <script type="text/javascript">
       function initialize() {
       	var event_address = "<?php echo $event->address;?>";
-            var event_geocoder = new google.maps.Geocoder();
-            event_geocoder.geocode( { 'address': event_address}, function(results, status) {
-              if (status == google.maps.GeocoderStatus.OK) {
-                
-                	window.eventlat = results[0].geometry.location.lat();
-                	window.eventlng = results[0].geometry.location.lng();
-
-              } else {
-                alert("Geocode for Address was not successful for the following reason: " + status);
-              }
-            });
-            console.log(window.eventlat);
-        var mapOptions = {
-          center: { lat: window.eventlat, lng: window.eventlng},
+      	var mapOptions = {
+          center: { lat: 43.6509618, lng: -79.3824327 },
           zoom: 12,
           scrollwheel: false,
           styles: [{"featureType":"landscape","stylers":[{"hue":"#FFBB00"},{"saturation":43.400000000000006},{"lightness":37.599999999999994},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#FFC200"},{"saturation":-61.8},{"lightness":45.599999999999994},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":51.19999999999999},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":52},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#0078FF"},{"saturation":-13.200000000000003},{"lightness":2.4000000000000057},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#00FF6A"},{"saturation":-1.0989010989011234},{"lightness":11.200000000000017},{"gamma":1}]}]
         };
         var map = new google.maps.Map(document.getElementById('map-canvas'),
             mapOptions);
+            var event_geocoder = new google.maps.Geocoder();
+            event_geocoder.geocode( { 'address': event_address}, function(results, status) {
+              if (status == google.maps.GeocoderStatus.OK) {
+                resultsMap.setCenter(results[0].geometry.location);
+			      var marker = new google.maps.Marker({
+			        map: resultsMap,
+			        position: results[0].geometry.location
+			      });
+
+              } else {
+                alert("Geocode for Address was not successful for the following reason: " + status);
+              }
+            });
+   
       }
       google.maps.event.addDomListener(window, 'load', initialize);
     </script>
