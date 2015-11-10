ec3cae56e442d8974279fee916a575d42262846c
Weipeng Wu <wwp722@gmail.com>
2015-11-10 15:53:06 -0500

    city fix

--- resources/views/events/new.blade.php
+++ resources/views/events/new.blade.php
@@ -131,7 +131,7 @@
       window.alert("Autocomplete's returned place contains no geometry");
       return;
     }
-    console.log(place.address_components[2].long_name);
+    console.log(place.address_components);
     // If the place has a geometry, then present it on a map.
     if (place.geometry.viewport) {
       map.fitBounds(place.geometry.viewport);
