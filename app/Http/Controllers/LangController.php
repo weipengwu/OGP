fd164f97d9198a135b003de1cee7f2eb6656d213
Weipeng Wu <wwp722@gmail.com>
2015-11-22 19:46:53 -0500

    fixes

--- app/Http/Controllers/LangController.php
+++ app/Http/Controllers/LangController.php
@@ -3,32 +3,7 @@
 
 class LangController extends Controller {
 
-	/*
-	|--------------------------------------------------------------------------
-	| Welcome Controller
-	|--------------------------------------------------------------------------
-	|
-	| This controller renders the "marketing page" for the application and
-	| is configured to only allow guests. Like most of the other sample
-	| controllers, you are free to modify or remove it as you desire.
-	|
-	*/
 
-	/**
-	 * Create a new controller instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		$this->middleware('guest');
-	}
-
-	/**
-	 * Show the application welcome screen to the user.
-	 *
-	 * @return Response
-	 */
 	public function switchLang($lang)
 	{
 		Session::set('applocale', $lang);
