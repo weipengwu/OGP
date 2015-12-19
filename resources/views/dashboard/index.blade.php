e894367bbc734bb11d141a56178af743674f4497
Weipeng Wu <wwp722@gmail.com>
2015-12-18 20:28:48 -0500

    debug

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -106,7 +106,8 @@
 						<input type="text" class="form-control" name="useremail" value="{{ getAuthoremail($id) }}" placeholder="Email">
 					</div>
 					<div class="form-group">
-						<textarea name="desc" class="form-control" placeholder="Bio">{!! strip_tags(userDesc(Auth::user()->id)[0]) !!}</textarea>
+						<?php var_dump(userDesc(Auth::user()->id));?>
+						<textarea name="desc" class="form-control" placeholder="Bio">{!! strip_tags(userDesc(Auth::user()->id)) !!}</textarea>
 					</div>
 					<input type="submit" class="btn btn-logo" value="Submit">
 				</form>
