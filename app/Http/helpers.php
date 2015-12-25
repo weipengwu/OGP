c8c3392badd841dd8cd25a5b7bc55fcbf503da31
Weipeng Wu <wwp722@gmail.com>
2015-12-25 10:41:11 -0500

    try slug

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -97,8 +97,15 @@
 		return $excerpt;
 	}
 
-	function slug($string){
-	    return strtolower(trim(preg_replace('~[^0-9a-z]+~i', '-', html_entity_decode(preg_replace('~&([a-z]{1,2})(?:acute|cedil|circ|grave|lig|orn|ring|slash|th|tilde|uml);~i', '$1', htmlentities($string, ENT_QUOTES, 'UTF-8')), ENT_QUOTES, 'UTF-8')), '-'));
+	// function slug($string){
+	//     return strtolower(trim(preg_replace('~[^0-9a-z]+~i', '-', html_entity_decode(preg_replace('~&([a-z]{1,2})(?:acute|cedil|circ|grave|lig|orn|ring|slash|th|tilde|uml);~i', '$1', htmlentities($string, ENT_QUOTES, 'UTF-8')), ENT_QUOTES, 'UTF-8')), '-'));
+	// }
+	function slug($string, $separator = '-') {
+	    $re = "/(\\s|\\".$separator.")+/mu";
+	    $str = @trim($string);
+	    $subst = $separator;
+	    $result = preg_replace($re, $subst, $str);
+	    return strtolower($result);
 	}
 
 	function getGroupName($id){
