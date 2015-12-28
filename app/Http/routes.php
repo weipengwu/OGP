9fc659051ea2604014c1849ce57ab8d7324a0626
Weipeng Wu <wwp722@gmail.com>
2015-12-28 17:18:19 -0500

    fix

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -42,8 +42,8 @@ Route::group(['middleware' => 'auth'], function(){
 	Route::get('groups/leave', array('uses' => 'GroupController@leaveGroup', 'as' => 'leaveGroup'));
 	Route::get('dashboard', array('uses' => 'DashboardController@index','as' => 'dashboard'));
 	Route::post('dashboard', array('uses' => 'DashboardController@updateProfile','as' => 'updateProfile'));
-	Route::get('changepassword', 'PasswordController@changepwdindex');
-	Route::post('changepassword', array('uses' => 'DashboardController@changePassword','as' => 'changePassword'));
+	Route::get('changepassword', 'Auth\PasswordController@changepwdindex');
+	Route::post('changepassword', array('uses' => 'Auth\PasswordController@changePassword','as' => 'changePassword'));
 	Route::get('groups/{slug}/posts/new', array('uses' => 'PostController@newPost', 'as' => 'newPost'));
 	// Route::post('groups/{slug}/posts/new', array('uses' => 'PostController@createPost', 'as' => 'createPost'));
 	Route::post('posts/new', array('uses' => 'PostController@createPost', 'as' => 'createPost'));
