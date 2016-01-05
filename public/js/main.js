3f401adb8eafbe1d2071bdb7a3404da5bd8ea5b5
Weipeng Wu <wwp722@gmail.com>
2016-01-04 22:42:29 -0500

    fix

--- public/js/main.js
+++ public/js/main.js
@@ -453,8 +453,7 @@ $(document).ready(function(){
 	$('.bxslider').bxSlider({
 	  adaptiveHeight: true,
 	  mode: 'fade',
-	  auto: true,
-	  infiniteLoop: false,
+	  auto: false,
 	  pager: false
 	});
 
