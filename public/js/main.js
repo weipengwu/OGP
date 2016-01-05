f304c8395660546db975f7f923ef4a459fdc5b84
Weipeng Wu <wwp722@gmail.com>
2016-01-05 00:12:40 -0500

    fix

--- public/js/main.js
+++ public/js/main.js
@@ -428,11 +428,11 @@ $(document).ready(function(){
 	
 	$(window).scroll(function(){
 		if($(this).scrollTop() > 465){
-			// $('.bannerwrapper').addClass('locked');
+			$('.bannerwrapper').addClass('locked');
 			// $('.bannerwrapper').next().css('margin-top', '485px');
 			$('.statusbar').addClass('locked');
 		}else{
-			// $('.bannerwrapper').removeClass('locked');
+			$('.bannerwrapper').removeClass('locked');
 			// $('.bannerwrapper').next().css('margin-top', '0');
 			$('.statusbar').removeClass('locked');
 		}
