289cca266a9d7dd47aaaf827f8dc7cac0d63f2be
Weipeng Wu <wwp722@gmail.com>
2016-01-08 14:33:09 -0500

    fix search

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -10,35 +10,28 @@
 					<h1>SEACH RESULTS</h1>
 				</div>
 				<div class="panel-body">
-				<pre>
-				<?php
-var_dump($groups);
-
-				?>
-				</pre>
-				<?php die();?>
 					@if (count($groups) > 0)
 						<h3>Brands</h3>
 						@foreach ($groups as $group)
 							<div class="singlepost">
-								<a href="{{ url() }}/brands/{{ $group->slug }}"><h4>{{ $group->name }}</h4></a>
-								<p>{{ getExcerpt($group->description) }}</p>
+								<a href="{{ url() }}/brands/{{ $group['slug'] }}"><h4>{{ $group['name'] }}</h4></a>
+								<p>{{ getExcerpt($group['description']) }}</p>
 							</div>
 						@endforeach
 					@elseif (count($posts) > 0)
 						<h3>Posts</h3>
 						@foreach ($posts as $post)
 							<div class="singlepost">
-								<a href="{{ url() }}/posts/{{ $post->id }}"><h4>{{ $post->title }}</h4></a>
-								<p>{{ getExcerpt($post->content) }}</p>
+								<a href="{{ url() }}/posts/{{ $post['id'] }}"><h4>{{ $post['title'] }}</h4></a>
+								<p>{{ getExcerpt($post['content']) }}</p>
 							</div>
 						@endforeach
 					@elseif (count($events) > 0)
 						<h3>Events</h3>
 						@foreach ($events as $event)
 							<div class="singlepost">
-								<a href="{{ url() }}/events/{{ $event->id }}"><h4>{{ $event->title }}</h4></a>
-								<p>{{ getExcerpt($event->content) }}</p>
+								<a href="{{ url() }}/events/{{ $event['id'] }}"><h4>{{ $event['title'] }}</h4></a>
+								<p>{{ getExcerpt($event['content']) }}</p>
 							</div>
 						@endforeach
 					@else
