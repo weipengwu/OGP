31e6adf5bab93a9cd5054e9fbc51dcd5e7cf0720
Weipeng Wu <wwp722@gmail.com>
2016-01-16 16:31:05 -0500

    fix

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -5,7 +5,7 @@
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
 			<div class="panel panel-default" style="margin-top: 50px">
-				<div class="panel-heading">Reset Password</div>
+				<div class="panel-heading">{{ trans('auth.resetpassword') }}</div>
 				<div class="panel-body">
 					@if (session('status'))
 						<div class="alert alert-success">
@@ -28,7 +28,7 @@
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
+							<label class="col-md-4 control-label">{{ trans('auth.emailaddress') }}</label>
 							<div class="col-md-6">
 								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
 							</div>
@@ -37,7 +37,7 @@
 						<div class="form-group">
 							<div class="col-md-6 col-md-offset-4">
 								<button type="submit" class="btn btn-logo">
-									Send Password Reset Link
+									{{ trans('auth.passwordresetlink') }}
 								</button>
 							</div>
 						</div>
