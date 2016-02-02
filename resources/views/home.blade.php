a99b8b18e22d1500363341059e2f1210d5958f83
Weipeng Wu <wwp722@gmail.com>
2016-02-01 23:48:59 -0500

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -80,7 +80,7 @@
 						<script type="text/javascript">
 							$('.right #post{{$post->id}} a.social_fb').on('click', function(e){
 								e.preventDefault();
-								window.open('https://www.facebook.com/v2.0/dialog/feed?app_id=866884463391641&display=popup&link='+encodeURIComponent('{{ url() }}/posts/<?php echo $post->id; ?>')+'&caption=OHGOODPARTY&picture={{ url()."/uploads/Medium_".$banner[0] }}&name='+encodeURIComponent('{{ $post->title }}')+'&description={!! html_entity_decode( trim(preg_replace("/\s+/", " ", getExcerpt($post->content, 60))) ) !!}&redirect_uri=https://www.facebook.com', "_blank", "width=360, height=360");
+								window.open('https://www.facebook.com/v2.0/dialog/feed?app_id=866884463391641&display=popup&link='+encodeURIComponent('{{ url() }}/posts/<?php echo $post->id; ?>')+'&caption=OHGOODPARTY&picture={{ url()."/uploads/Medium_".$banner[0] }}&name='+encodeURIComponent('{{ $post->title }}')+'&description={{ strip_tags( trim(preg_replace("/\s+/", " ", getExcerpt($post->content, 60))) ) }}&redirect_uri=https://www.facebook.com', "_blank", "width=360, height=360");
 							})
 							$('.right #post{{$post->id}} a.social_tw').on('click', function(e){
 								e.preventDefault();
@@ -88,7 +88,7 @@
 							})
 							$('.right #post{{$post->id}} a.social_lk').on('click', function(e){
 								e.preventDefault();
-								window.open('https://www.linkedin.com/shareArticle?mini=true&url={{ url() }}/posts/<?php echo $post->id; ?>&title={{ $post->title }}&summary={!! html_entity_decode( trim(preg_replace("/\s+/", " ", getExcerpt($post->content, 60))) ) !!}&source=OHGOODPARTY', "_blank", "width=360, height=360");
+								window.open('https://www.linkedin.com/shareArticle?mini=true&url={{ url() }}/posts/<?php echo $post->id; ?>&title={{ $post->title }}&summary={{ strip_tags(getExcerpt($post->content, 60)) }}&source=OHGOODPARTY', "_blank", "width=360, height=360");
 							})
 							$('.right #post{{$post->id}} a.social_wb').on('click', function(e){
 								e.preventDefault();
