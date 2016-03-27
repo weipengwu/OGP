2255f23297d79be1ea4e9223658e6cc9e1a40a17
Weipeng Wu <wwp722@gmail.com>
2016-03-27 01:36:51 -0400

    add GA

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -133,6 +133,16 @@
 	<script src="{{ asset('/js/tinymce/tinymce.min.js') }}"></script>
 	<script src="{{ asset('/js/jstz.min.js') }}"></script>
 	<script src="{{ asset('/js/main.js') }}"></script>
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-75615748-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </body>
 </html>
 
